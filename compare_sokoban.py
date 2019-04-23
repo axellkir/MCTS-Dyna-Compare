cdcaac34ebfbb73df402964bdc41a573eb31acde
AxellKir <akskir@gmail.com>
2019-04-23 13:49:53 +0300

    ADD: Sokoban compare file

--- compare_sokoban.py
+++ compare_sokoban.py
@@ -1,2 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+from agents.dyna_q import DynaQAgent
+from agents.dyna_2 import Dyna2Agent
+from agents.uct import UCTAgent
+from envs.sokoban_env import MySokobanEnv
 
 
+def moving_average(x, span=100): return DataFrame(
+    {'x': np.asarray(x)}).x.ewm(span=span).mean().values
+
+
+env = MySokobanEnv()
+n_actions = env.action_space.n
+env.render()
+s = env.reset()
+
+agent_dyna_q = DynaQAgent(alpha=0.6, epsilon=0.15, discount=0.9, n_steps=5000,
+                          get_legal_actions=lambda s: range(n_actions), is_array=True)
+
+agent_dyna_2 = Dyna2Agent(get_legal_actions=lambda s: range(n_actions), is_array=True)
+
+agent_uct = UCTAgent(get_legal_actions=lambda s: range(n_actions), is_copy=False, save=lambda env: env.save_room,
+                     restore=lambda env: env.restore_room)
+agent_uct.set_root(s)
+
+rewards_dyna_q = []
+rewards_dyna_2 = []
+rewards_uct = []
+
+for i in range(400):
+    #     rewards_dyna_q.append(agent_dyna_q.play(env))
+    rewards_dyna_2.append(agent_dyna_2.play(env))
+    #     rewards_uct.append(agent_uct.play(env))
+    if i % 10 == 0:
+        #         print('DYNA-Q mean reward =', np.mean(rewards_dyna_q[-100:]))
+        print('DYNA-2 mean reward =', np.mean(rewards_dyna_2[-100:]))
+        #         print('UCT mean reward =', np.mean(rewards_uct[-100:]))
+
+        #         plt.plot(moving_average(rewards_dyna_q), label='dyna-q')
+        plt.plot(moving_average(rewards_dyna_2), label='dyna-2')
+        #         plt.plot(moving_average(rewards_uct), label='uct')
+
+        plt.grid()
+        plt.legend()
+        plt.show()
--- compare_taxi.py
+++ compare_taxi.py
@@ -5,38 +5,21 @@ from agents.dyna_q import DynaQAgent
 from agents.uct import UCTAgent
 from agents.qlearning import QLearningAgent
 from envs.taxi_env import TaxiEnv
+from copy import deepcopy
 
 env = TaxiEnv(1)
 n_actions = env.action_space.n
 env.render()
 s = env.reset()
 
-agent_uct = UCTAgent(get_legal_actions=lambda s: range(n_actions))
+agent_uct = UCTAgent(get_legal_actions=lambda s: range(n_actions), copy=deepcopy, is_copy=True)
 agent_uct.set_root(s)
 
 agent_ql = QLearningAgent(alpha=0.25, epsilon=0.05, discount=0.9,
                           get_legal_actions=lambda s: range(n_actions))
 
-agent_dyna_q = DynaQAgent(alpha=0.25, epsilon=0.05, discount=0.9, n_steps = 200,
+agent_dyna_q = DynaQAgent(alpha=0.25, epsilon=0.05, discount=0.9, n_steps=200,
                           get_legal_actions=lambda s: range(n_actions))
-
-
-def play_and_train(env, agent, t_max=10**4):
-    total_reward = 0.0
-    state = env.reset()
-
-    for t in range(t_max):
-        a = agent.get_action(state)
-
-        next_state, r, done, _ = env.step(a)
-        agent.update(state, a, r, next_state)
-
-        state = next_state
-        total_reward += r
-        if done:
-            break
-
-    return total_reward
 
 
 def moving_average(x, span=100): return DataFrame(
@@ -46,8 +29,8 @@ def moving_average(x, span=100): return DataFrame(
 rewards_dyna_q, rewards_ql, rewards_uct = [], [], []
 
 for i in range(50):
-    rewards_dyna_q.append(play_and_train(env, agent_dyna_q, 200))
-    rewards_ql.append(play_and_train(env, agent_ql, 200))
+    rewards_dyna_q.append(agent_dyna_q.play(env, 200))
+    rewards_ql.append(agent_ql.play(env, 200))
     rewards_uct.append(agent_uct.play(env, 200))
 
 print('UCT mean reward =', np.mean(rewards_uct[-100:]))
