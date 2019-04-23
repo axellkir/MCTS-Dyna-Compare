b3cc0c31191e85f81f26e7331e6546cae4d8b2fd
AxellKir <akskir@gmail.com>
2019-04-23 13:50:51 +0300

    ADD: possibility to save and restore sokoban env

--- envs/sokoban_env.py
+++ envs/sokoban_env.py
@@ -1,9 +1,8 @@
-from gym.spaces.discrete import Discrete
-from gym.spaces import Box
-from gym_sokoban.envs.render_utils import room_to_rgb, room_to_tiny_world_rgb
-from gym_sokoban.envs.room_utils import generate_room
 import numpy as np
 from gym_sokoban.envs import SokobanEnv
+from gym.spaces.discrete import Discrete
+from gym.spaces import Box
+from gym_sokoban.envs.room_utils import generate_room
 
 
 class MySokobanEnv(SokobanEnv):
@@ -24,7 +23,7 @@ class MySokobanEnv(SokobanEnv):
 
         # Penalties and Rewards
         self.penalty_for_step = -0.1
-        self.penalty_box_off_target = -1
+        self.penalty_box_off_target = -0.1
         self.reward_box_on_target = 1
         self.reward_finished = 10
         self.reward_last = 0
@@ -39,6 +38,41 @@ class MySokobanEnv(SokobanEnv):
         # Initialize Room
         self.generate()
         self.reset()
+
+    def step(self, action):
+        assert action in ACTION_LOOKUP
+
+        self.num_env_steps += 1
+
+        self.new_box_position = None
+        self.old_box_position = None
+
+        moved_player = False
+        moved_box = False
+        # All push actions are in the range of [0, 3]
+        if action < 4:
+            moved_player, moved_box = self._push(action)
+
+        else:
+            moved_player = self._move(action)
+
+        self._calc_reward()
+
+        done = self._check_if_done()
+
+        # Convert the observation to RGB frame
+        #         observation = self.render(mode='rgb_array')
+
+        info = {
+            "action.name": ACTION_LOOKUP[action],
+            "action.moved_player": moved_player,
+            "action.moved_box": moved_box,
+        }
+        if done:
+            info["maxsteps_used"] = self._check_if_maxsteps()
+            info["all_boxes_on_target"] = self._check_if_all_boxes_on_target()
+
+        return self.room_state, self.reward_last, done, info
 
     def generate(self, second_player=False):
         try:
@@ -56,14 +90,21 @@ class MySokobanEnv(SokobanEnv):
         self.player_position = np.argwhere(self.room_state == 5)[0]
         self.init_room = (self.room_fixed, self.room_state, self.box_mapping, self.player_position)
 
-    def reset(self, second_player=False):
+    def save_room(self):
+        self.whole_env = (self.room_fixed, self.room_state, self.box_mapping, self.player_position)
+        self.env_params = (self.num_env_steps, self.reward_last, self.boxes_on_target)
+
+    def restore_room(self):
+        (self.room_fixed, self.room_state, self.box_mapping, self.player_position) = self.whole_env
+        (self.num_env_steps, self.reward_last, self.boxes_on_target) = self.env_params
+
+    def reset(self):
         (self.room_fixed, self.room_state, self.box_mapping, self.player_position) = self.init_room
         self.num_env_steps = 0
         self.reward_last = 0
         self.boxes_on_target = 0
 
-        starting_observation = room_to_rgb(self.room_state, self.room_fixed)
-        return starting_observation
+        return self.room_state
 
 
 ACTION_LOOKUP = {
